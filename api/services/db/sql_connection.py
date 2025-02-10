e735d398606d037f9b94b6e17433cdad5319c7a5
Gabriel Siqueira <gabrielss2406@gmail.com>
2025-02-10 14:53:28 -0300

    fix: database_url

--- api/services/db/sql_connection.py
+++ api/services/db/sql_connection.py
@@ -1,24 +1,13 @@
 import psycopg2
-import os
 from dotenv import load_dotenv
 
 load_dotenv()
-
-SUPABASE_USER = os.getenv("SUPABASE_USER")
-SUPABASE_PASSWORD = os.getenv("SUPABASE_PASSWORD")
-SUPABASE_HOST = os.getenv("SUPABASE_HOST")
-SUPABASE_PORT = os.getenv("SUPABASE_PORT")
-SUPABASE_DATABASE = os.getenv("SUPABASE_DATABASE")
 
 
 def connect():
     try:
         connection = psycopg2.connect(
-            user=SUPABASE_USER,
-            password=SUPABASE_PASSWORD,
-            host=SUPABASE_HOST,
-            port=SUPABASE_PORT,
-            database=SUPABASE_DATABASE
+            "postgresql://postgres:connectfarm@db.gxnlfyzuqhznrojmkegp.supabase.co:5432/postgres"
         )
         print("Conexão estabelecida com sucesso.")
         return connection
